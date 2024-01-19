24cfb5043b700ce6f6394664326b203ee1546c26
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-19 00:52:16 -1200

    feat: add entity for customer and product

--- src/main/java/com/example/ordersystem/entitys/Customer.java
+++ src/main/java/com/example/ordersystem/entitys/Customer.java
@@ -1,2 +1,33 @@
-package com.example.ordersystem.entitys;public class Customer {
+package com.example.ordersystem.entitys;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_sequence")
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+
+    private String email;
+
+    private String address;
 }

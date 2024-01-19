96197ef89fce04d7296431112665b790e1070f37
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-19 02:46:19 -1200

    feat: add customer controller for getting customers

--- src/main/java/com/example/ordersystem/controllers/CustomerController.java
+++ src/main/java/com/example/ordersystem/controllers/CustomerController.java
@@ -3,10 +3,9 @@ package com.example.ordersystem.controllers;
 import com.example.ordersystem.entitys.Customer;
 import com.example.ordersystem.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
@@ -21,5 +20,10 @@ public class CustomerController {
     @PostMapping("/add")
     public Customer addCustomer(@RequestBody Customer customer){
         return customerService.AddCustomer(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
     }
 }

d838e48f88378822dc77f2a01fb42df082e62fb9
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-19 02:16:19 -1200

    feat: add customer repository interface

---
+++ src/main/java/com/example/ordersystem/Repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.ordersystem.Repositories;
+
+import com.example.ordersystem.entitys.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}

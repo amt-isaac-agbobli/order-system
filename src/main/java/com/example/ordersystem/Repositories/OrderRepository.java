a73cc88f3474a202cd409303e475ff4a1e3f5843
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-22 04:51:39 -1200

    Add @Repository annotation to OrderRepository

--- src/main/java/com/example/ordersystem/Repositories/OrderRepository.java
+++ src/main/java/com/example/ordersystem/Repositories/OrderRepository.java
@@ -2,9 +2,10 @@ package com.example.ordersystem.Repositories;
 
 import com.example.ordersystem.entitys.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByCustomerId(Long customerId);

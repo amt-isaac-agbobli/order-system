4f84ac12e3197c82df7b9e937d49260a11963a9e
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-22 04:25:12 -1200

    Add validation constraints to UpdateCustomerDetailsDto

--- src/main/java/com/example/ordersystem/dtos/UpdateCustomerDetailsDto.java
+++ src/main/java/com/example/ordersystem/dtos/UpdateCustomerDetailsDto.java
@@ -1,5 +1,6 @@
 package com.example.ordersystem.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,8 +14,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UpdateCustomerDetailsDto {
     @NotNull(message = "First name cannot be null")
+    @NotBlank(message = "First name cannot be blank")
     @Size(min = 5, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
+    @NotBlank(message = "Last name cannot be blank")
     @NotNull(message = "Last name cannot be null")
     @Size(min = 5, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;

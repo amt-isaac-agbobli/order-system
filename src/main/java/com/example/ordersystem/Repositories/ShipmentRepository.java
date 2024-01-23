1bc497c5a3164a948d028c967cad7b933875b810
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-23 00:53:36 -1200

    Refactor shipment repository and service classes

--- src/main/java/com/example/ordersystem/Repositories/ShipmentRepository.java
+++ src/main/java/com/example/ordersystem/Repositories/ShipmentRepository.java
@@ -1,2 +1,9 @@
-package com.example.ordersystem.Repositories;public interface ShipmentRepository {
+package com.example.ordersystem.Repositories;
+
+import com.example.ordersystem.entitys.Shipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 }
--- src/main/java/com/example/ordersystem/services/ShipmentService.java
+++ src/main/java/com/example/ordersystem/services/ShipmentService.java
@@ -1,2 +1,19 @@
-package com.example.ordersystem.services;public class ShipmentService {
+package com.example.ordersystem.services;
+
+import com.example.ordersystem.Repositories.OrderRepository;
+import com.example.ordersystem.Repositories.ShipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShipmentService {
+    private final ShipmentRepository shipmentRepository;
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public ShipmentService(ShipmentRepository shipmentRepository, OrderRepository orderRepository) {
+        this.shipmentRepository = shipmentRepository;
+        this.orderRepository = orderRepository;
+    }
+    
 }
